6239033cadcacc91d0dab6f541340eb5071c68d5
thearjungp <54946157+thearjungp@users.noreply.github.com>
2021-03-13 12:37:44 +0530

    modified

--- palettegenerate.js
+++ palettegenerate.js
@@ -10,17 +10,17 @@ function generatePalette()
 
   
   //shades
-  document.getElementById("color1").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (l-30) +"%)";
-  document.getElementById("color2").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (l-20) +"%)";
-  document.getElementById("color3").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (l-10) +"%)";
+  document.getElementById("color1").style.backgroundColor = "hsl("+ h + "," + (+s*1.15) + "%," + (l-30) +"%)";
+  document.getElementById("color2").style.backgroundColor = "hsl("+ h + "," + (+s*1.10) + "%," + (l-20) +"%)";
+  document.getElementById("color3").style.backgroundColor = "hsl("+ h + "," + (+s*1.05) + "%," + (l-10) +"%)";
 
-  // base color
+  //base color
   document.getElementById("color4").style.backgroundColor = "hsl("+ h + "," + s + "%," + l +"%)";
 
   //tint
-  document.getElementById("color5").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (+l+10) +"%)";
-  document.getElementById("color6").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (+l+20) +"%)";
-  document.getElementById("color7").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (+l+30) +"%)";
-  document.getElementById("color8").style.backgroundColor = "hsl("+ h + "," + (+s) + "%," + (+l+40) +"%)";
+  document.getElementById("color5").style.backgroundColor = "hsl("+ h + "," + (+s*0.83) + "%," + (+l+10) +"%)";
+  document.getElementById("color6").style.backgroundColor = "hsl("+ h + "," + (+s*0.69) + "%," + (+l+20) +"%)";
+  document.getElementById("color7").style.backgroundColor = "hsl("+ h + "," + (+s*0.57) + "%," + (+l+30) +"%)";
+  document.getElementById("color8").style.backgroundColor = "hsl("+ h + "," + (+s*0.47) + "%," + (+l+40) +"%)";
 
 }
